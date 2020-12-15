efd831f6a764e05483d2ba21c81c606941f496ee
alimertcetin <75283633+alimertcetin@users.noreply.github.com>
2020-12-15 17:08:46 +0300

    Folders are organized - Some colliders are added
    Proje dosyaları düzenlendi. Yeni harita için import edilen(couch, chair, stair, lamp vs.) assetlerin bazılarına collider eklendi. Light baking denendi ancak public bathroom kısmındaki zemin ışık pişirilirken lavabonun gölgesini üzerine almıyor veya lavabo ışık pişirilirken gölge yansıtmıyor.

---
+++ Sistem Analizi/Assets/Scripts/Keycard_Script.cs
@@ -0,0 +1,105 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public enum KeycardLevel
+{
+    Seivye_1,
+    Seivye_2,
+    Seivye_3
+}
+
+public class Keycard_Script : MonoBehaviour
+{
+    const string newLine = "\r\n";
+    instance_Player_Inventory inventory;
+
+    [Tooltip("Kartın kaçıncı seviye kart olduğunu belirleyin." + newLine +
+        "1.Seviye : Yeşil," + newLine +
+        "2.Seviye : Sarı," + newLine +
+        "3.Seviye : Kırmızı")]
+    public KeycardLevel Keycard;
+
+    [HideInInspector]
+    public bool Selected_Keycard1 = false,
+    Selected_Keycard2 = false,
+    Selected_Keycard3 = false;
+
+    [Tooltip("Toplandığında oynatılacak particle efektini bu alana ekleyin.")]
+    [SerializeField]
+    ParticleSystem CollectedParticle = null;
+
+    private void SetCurrentCardState()
+    {
+        //Keycard'ın default değeri 0 olduğundan cast (int)Keycard VS tarafından gereksiz bulunacaktır.
+        //(int)Keycard == (int)KeycardLevel.Seivye_1
+        if (Keycard == KeycardLevel.Seivye_1) //Cast etmeye de gerek yok zaten.
+        {
+            Selected_Keycard1 = true;
+            Selected_Keycard2 = false;
+            Selected_Keycard3 = false;
+        }
+        else if (Keycard == KeycardLevel.Seivye_2)
+        {
+            Selected_Keycard1 = false;
+            Selected_Keycard2 = true;
+            Selected_Keycard3 = false;
+        }
+        else if (Keycard == KeycardLevel.Seivye_3)
+        {
+            Selected_Keycard1 = false;
+            Selected_Keycard2 = false;
+            Selected_Keycard3 = true;
+        }
+        else
+        {
+            Debug.LogWarning("Kartlarda bi gariplik var." + this.name);
+        }
+    }
+
+    private void Awake()
+    {
+        SetCurrentCardState();
+
+        inventory = FindObjectOfType<instance_Player_Inventory>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Selected_Keycard1)
+            {
+                inventory.CollectedKeycard("green");
+
+                SpawnParicle();
+
+                Destroy(this.gameObject);
+            }
+            else if (Selected_Keycard2)
+            {
+                inventory.CollectedKeycard("Yellow");
+
+                SpawnParicle();
+
+                Destroy(this.gameObject);
+            }
+            else if (Selected_Keycard3)
+            {
+                inventory.CollectedKeycard("red");
+
+                SpawnParicle();
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void SpawnParicle()
+    {
+        GameObject go = Instantiate(CollectedParticle.gameObject); //Particle yarat.
+        go.transform.position = this.transform.position; //Particle konumunu bu Gameobject olarak belirle
+        Destroy(go, 5.0f); //Particle'ı 5 saniye sonra yok et.
+    }
+    
+
+}
